c398e3890cdfbe31c73e9c8feee0f91f098c6a98
Ian Kelly <ian.aloysious.kelly@gmail.com>
2017-08-03 09:27:19 -0500

    Uodated with seperate files

---
+++ SpringBoot.java
@@ -0,0 +1,16 @@
+package netgloo.controllers;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+  @RequestMapping("/")
+  // @ResponseBody CodePilot
+  public String index() {
+    return "hello.html";
+  }
+
+}
